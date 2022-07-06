421b79aef96b95c6592453d72ae146cd1250820f
Freddo1864 <freddo1864@gmail.com>
2022-07-06 15:55:54 +0200

    06/07   15.55
    Etape 2 Exos Canvas

--- JeuCanvas/canvas.js
+++ JeuCanvas/canvas.js
@@ -1,23 +1,32 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+/* Définition de la position de départ de la balle */
+var x = canvas.width/2;
+var y = canvas.height-30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+/* Variables déclarées pour être utilisées dans la fonction (draw) qui permettra de faire déplacer la balle */
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function drawBall(){
+      ctx.beginPath();
+      ctx.arc(x, y, 10, 0, Math.PI*2);
+      ctx.fillStyle = "#0095DD"
+      ctx.fill();
+      ctx.closePath();
+}
+
+/* Fonction permettant de déplacer le cercle (la balle) */
+function draw() {
+      /* Ajout de ctx.clearRect pour effacer la balle après chaque déplacement */
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawBall();
+      x += dx;
+      y += dy;
+}
+setInterval(draw, 10);
+
 
 
 
