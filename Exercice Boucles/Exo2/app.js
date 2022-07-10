282c089ac56235b2379569fe133c20a01ab300f5
Freddo1864 <freddo1864@gmail.com>
2022-07-10 17:49:45 +0200

    10/07   17.47
    Exos sur les tableaux
    Méthodes :
    .push, .pop, .shift, .indexOf, .splice, .length, .join, .split

--- Exercice Boucles/Exo2/app.js
+++ Exercice Boucles/Exo2/app.js
@@ -1,11 +1,26 @@
 var tab = new Array();
-tab[0] = "Lundi";
-tab[1] = "Mardi";
-tab[2] = "Mercredi";
-tab[3] = "Jeudi";
-tab[4] = "Vendredi";
-tab[5] = "Samedi";
-tab[6] = "Dimanche";
+tab["Lundi"] = "Ecole";
+tab["Mardi"] = "Ecole";
+tab["Mercredi"] = "Repos";
+tab["Jeudi"] = "Ecole";
+tab["Vendredi"] = "Ecole";
+tab["Samedi"] = "Football";
+tab["Dimanche"] = "Repos";
 var longueur = tab.length;
 
+console.table(tab);
 
+function displayJour(j) {
+      console.log("Jour " + j + " = " +tab[j]);
+}
+
+// var tab2 = new Array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+// var tab3=["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+// var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet", "Août","Septembre","Octobre","Novembre","Décembre"];
+
+// console.log(mois);
+// console.log(tab3);
+
+var j = 1;
+document.write("Le jour à l'indice " + j + " est " + tab[j]);
+document.write(". C'est un jour de " + tabPage[tab[j]]);
