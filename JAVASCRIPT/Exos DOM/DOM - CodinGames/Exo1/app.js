edcf1bd246903f9b1e636024c4d83f0aa031d628
Freddo1864 <freddo1864@gmail.com>
2022-07-11 16:53:20 +0200

    11/07   16.52
    Exos DOM  codinGames

---
+++ JAVASCRIPT/Exos DOM/DOM - CodinGames/Exo1/app.js
@@ -0,0 +1,13 @@
+// var titre = document.querySelector("h1");
+// console.log(titre.textContent);
+// document.querySelector("h1").textContent = "Rick Astley - Never Gonna Give You Up";
+// console.log(titre.textContent);
+let texte = document.body.firstElementChild.innerHTML;
+document.body.firstElementChild.innerHTML = "Rick Astley - Never Gonna Give You Up";
+
+let elements = document.getElementsByClassName("couplet");
+elements[0].innerHTML = ""
+
+
+
+
