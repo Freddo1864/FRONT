2941a9200ea189a3cad29aab113a007a13067d28
Freddo1864 <freddo1864@gmail.com>
2022-07-12 16:02:04 +0200

    12/07   16.01
    Exo 1 et Exo 2
    codinGames

---
+++ JAVASCRIPT/Exos DOM/DOM - CodinGames/Exo2/app.js
@@ -0,0 +1,8 @@
+// Supprimer la classe "red" avec Jquery
+$("#paroles .red").removeClass("red");
+
+// Mettre en italique tous les refrains
+$("#paroles .refrain").addClass("italic");
+
+// Change la couleur du background
+$("#paroles").css("background-color" , "#6495ED");
