aa896fd14e12aae6d4af682385ae99e6f9169c45
Freddo1864 <106061276+Freddo1864@users.noreply.github.com>
2022-08-21 11:09:41 +0200

    21/08   11.08
    Cours React 
    Vidéo FromScratch
    Site avec les pays....

---
+++ JAVASCRIPT/cours-fromscratch/src/pages/About.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+      <div>
+        <Logo />
+        <Navigation />
+        <h1>A Propos</h1>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+          alias rem numquam sapiente corrupti quam possimus incidunt, modi quae
+          reiciendis sit beatae quas nulla et libero molestiae eligendi,
+          repellat blanditiis dicta ab voluptas. Autem optio perferendis tenetur
+          quasi quod eius consequuntur aspernatur excepturi harum velit
+          doloribus, iste tempore aut animi corporis magnam provident libero
+          distinctio dolorem! Quisquam consectetur dicta consequuntur
+          perspiciatis, molestiae, doloribus numquam rem magnam vitae harum
+          architecto voluptates saepe, suscipit aliquid impedit eos sequi dolor
+          sed. Quod ea animi tenetur consectetur? Itaque eum aliquam, repellat
+          quod veritatis quae enim sapiente odit, earum porro in rerum doloribus
+          quasi ducimus.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa optio
+          necessitatibus ex enim repudiandae, minima iste odio, voluptates neque
+          molestias dolorum ea quidem voluptatibus culpa eum repellat? Officiis,
+          et! Iure dolorum est beatae ullam obcaecati voluptates corporis quae
+          id incidunt quod iste maiores dolorem necessitatibus vitae aliquam at
+          facere reprehenderit quam ducimus reiciendis, dolor consequatur ad
+          voluptatum. Assumenda quia repellat neque et, recusandae asperiores
+          odio eaque vero facilis quibusdam sunt! Harum itaque, repudiandae
+          accusantium libero optio recusandae quisquam ex sint eligendi
+          asperiores et, in obcaecati. Rem culpa enim itaque adipisci nisi, ea
+          repellendus exercitationem sapiente dolorum consectetur deleniti sit
+          voluptatibus.
+        </p>
+      </div>
+    );
+};
+
+export default About;
