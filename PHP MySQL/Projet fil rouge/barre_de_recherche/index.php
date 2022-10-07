363eb75b72a78848a986032ec15ac7f0464d1f3b
Freddo1864 <freddo1864@gmail.com>
2022-10-07 10:04:51 +0200

    07/10   10.04
    V1.1

--- PHP MySQL/Projet fil rouge/barre_de_recherche/index.php
+++ PHP MySQL/Projet fil rouge/barre_de_recherche/index.php
@@ -2,22 +2,22 @@
 // accès a la base de données achat_voiture.
     $bdd = new PDO('mysql:host=localhost;dbname=achat_voiture;', 'root', '');
 // Requête pour récupérer toutes les nom_voiture de la table voitures.
-    $allcars = $bdd->query('SELECT * FROM voitures ORDER BY nom_voiture');
+    $allcars = $bdd->query('SELECT * FROM donnees_de_base');
 // On vérifie si l'utilisateur a bien effectué sa recherche.
     if(isset($_GET['s']) AND !empty($_GET['s'])) {
 // On stocke la recherche de l'utilisateur dans la variable ==> $recherche.
         $recherche = htmlspecialchars($_GET['s']);
 // Requête pour vérifier dans la table le nom de la voiture entré par la recherche de l'utilisateur.
-        $allcars = $bdd->query('SELECT nom_voiture FROM voitures WHERE nom_voiture LIKE "%'.$recherche.'%" ORDER BY nom_voiture');
+        $allcars = $bdd->query('SELECT carrosserie, version_pays, nom_voiture FROM donnees_de_base NATURAL JOIN voitures WHERE carrosserie LIKE "%'.$recherche.'%"');
     }
 ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">    
     <title>Rechercher des utilisateurs</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form method="GET">
@@ -33,13 +33,22 @@
                 while($car = $allcars->fetch()) {
         ?>
 <!-- On affiche tous les véhicules dans un paragraphe. -->
-            <p><?php echo $car['nom_voiture'];?></p>
+            <table>
+                <tr><td>Carrosserie</td><td>Pays</td><td>Marque</td></tr>
+                <?php             
+                echo 
+                    $car['carrosserie']. ' ' .
+                    $car['version_pays']. ' ' .
+                    $car['nom_voiture'];
+                ?>
+            </table>
         <?php            
                 }
-            }else {
-                ?>
+            } else {
+
+        ?>
                     <p>Aucun véhicule trouvé</p>
-                <?php
+        <?php
             }
         ?>
     </section>
